01f64a9f86953b9789dfd8b2308aca5f823a0702
piotr.minkowski <piotr.minkowski@gmail.com>
2017-04-28 16:40:59 +0200

    test only

--- person-service/src/main/java/pl/piomin/services/datagrid/person/PersonApplication.java
+++ person-service/src/main/java/pl/piomin/services/datagrid/person/PersonApplication.java
@@ -1,7 +1,15 @@
 package pl.piomin.services.datagrid.person;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.jpa.HibernateEntityManagerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.SerializerConfig;
+import com.hazelcast.core.HazelcastInstance;
 
 @SpringBootApplication
 public class PersonApplication {
@@ -10,4 +18,24 @@ public class PersonApplication {
 		SpringApplication.run(PersonApplication.class, args);
 	}
 
+	@Bean
+	HazelcastInstance hazelcastInstance() {
+		ClientConfig config = new ClientConfig();
+		config.getGroupConfig().setName("dev").setPassword("dev-pass");
+		config.getNetworkConfig().addAddress("192.168.99.100:5701");
+		SerializerConfig sc = new SerializerConfig()
+				.setClassName("com.hazelcast.hibernate.serialization.Hibernate5CacheEntrySerializer")
+//			    .setImplementation(new Hibernate5CacheEntrySerializerHook().createSerializer())
+			    .setTypeClassName("pl.piomin.services.datagrid.person.model.Person");
+		config.getSerializationConfig().addSerializerConfig(sc);
+		
+		HazelcastInstance instance = HazelcastClient.newHazelcastClient(config);
+		return instance;
+	}
+	
+	@Bean  
+	public SessionFactory sessionFactory(HibernateEntityManagerFactory hemf){  
+	    return hemf.getSessionFactory();  
+	}
+	
 }
