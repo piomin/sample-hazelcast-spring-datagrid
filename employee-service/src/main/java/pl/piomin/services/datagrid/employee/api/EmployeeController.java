180b6132c71d89997039701277f698ac6f08695f
Piotr Mińkowski <piotr_minkowski@wp.pl>
2017-05-09 23:51:03 +0200

    working version

--- employee-service/src/main/java/pl/piomin/services/datagrid/employee/api/EmployeeController.java
+++ employee-service/src/main/java/pl/piomin/services/datagrid/employee/api/EmployeeController.java
@@ -1,84 +1,48 @@
 package pl.piomin.services.datagrid.employee.api;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
-import com.hazelcast.query.Predicate;
-import com.hazelcast.query.Predicates;
-
-import pl.piomin.services.datagrid.employee.data.EmployeeRepository;
 import pl.piomin.services.datagrid.employee.model.Employee;
+import pl.piomin.services.datagrid.employee.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
 
-	protected Logger logger = Logger.getLogger(EmployeeController.class.getName());
+	private Logger logger = Logger.getLogger(EmployeeController.class.getName());
 	
 	@Autowired
-	EmployeeRepository repository;
-	
-	@Autowired
-	HazelcastInstance instance;
-	IMap<Integer, Employee> map;
-	
-	@PostConstruct
-	public void init() {
-		map = instance.getMap("employee");
-		map.addIndex("company", true);
-		logger.info("Employees cache: " + map.size());
-
-	}
+	EmployeeService service;
 	
 	@GetMapping("/employees/person/{id}")
 	public Employee findByPersonId(@PathVariable("id") Integer personId) {
-		Predicate predicate = Predicates.equal("personId", personId);
-		Collection<Employee> ps = map.values(predicate);
-		logger.info("Employees: " + ps);
-		Optional<Employee> e = ps.stream().findFirst();
-		if (e.isPresent())
-			return e.get();
-		return repository.findByPersonId(personId);
+		logger.info(String.format("findByPersonId(%d)", personId));
+		return service.findByPersonId(personId);
 	}
 	
 	@GetMapping("/employees/company/{company}")
 	public List<Employee> findByCompany(@PathVariable("company") String company) {
-		Predicate predicate = Predicates.equal("company", company);
-		logger.info("Employees cache find");
-		Collection<Employee> ps = map.values(predicate);
-		logger.info("Employees cache size: " + ps.size());
-		if (ps.size() > 0) {
-			return ps.stream().collect(Collectors.toList());
-		}
-		logger.info("Employees find");
-		List<Employee> e = repository.findByCompany(company);
-		logger.info("Employees size: " + e.size());
-		e.parallelStream().forEach(it -> {
-			map.putIfAbsent(it.getId(), it);
-		});
-		return e;
+		logger.info(String.format("findByCompany(%s)", company));
+		return service.findByCompany(company);
 	}
 	
 	@GetMapping("/employees/{id}")
 	public Employee findById(@PathVariable("id") Integer id) {
-		Employee e = repository.findOne(id);
-		map.put(id, e);
-		return e;
+		logger.info(String.format("findById(%d)", id));
+		return service.findById(id);
 	}
 	
-	@GetMapping("/employees")
-	public List<Employee> findAll() {
-		return (List<Employee>) repository.findAll();
+	@PostMapping("/employees")
+	public Employee add(@RequestBody Employee emp) {
+		logger.info(String.format("add(%s)", emp));
+		return service.add(emp);
 	}
+	
 }
---
+++ employee-service/src/main/java/pl/piomin/services/datagrid/employee/service/EmployeeService.java
@@ -0,0 +1,90 @@
+package pl.piomin.services.datagrid.employee.service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.query.Predicate;
+import com.hazelcast.query.Predicates;
+
+import pl.piomin.services.datagrid.employee.data.EmployeeRepository;
+import pl.piomin.services.datagrid.employee.model.Employee;
+
+@Service
+public class EmployeeService {
+
+	private Logger logger = Logger.getLogger(EmployeeService.class.getName());
+
+	@Autowired
+	EmployeeRepository repository;
+	@Autowired
+	HazelcastInstance instance;
+	
+	IMap<Integer, Employee> map;
+	
+	@PostConstruct
+	public void init() {
+		map = instance.getMap("employee");
+		map.addIndex("company", true);
+		logger.info("Employees cache: " + map.size());
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public Employee findByPersonId(Integer personId) {
+		Predicate predicate = Predicates.equal("personId", personId);
+		logger.info("Employee cache find");
+		Collection<Employee> ps = map.values(predicate);
+		logger.info("Employee cached: " + ps);
+		Optional<Employee> e = ps.stream().findFirst();
+		if (e.isPresent())
+			return e.get();
+		logger.info("Employee cache find");
+		Employee emp = repository.findByPersonId(personId);
+		logger.info("Employee: " + emp);
+		map.put(emp.getId(), emp);
+		return emp;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public List<Employee> findByCompany(String company) {
+		Predicate predicate = Predicates.equal("company", company);
+		logger.info("Employees cache find");
+		Collection<Employee> ps = map.values(predicate);
+		logger.info("Employees cache size: " + ps.size());
+		if (ps.size() > 0) {
+			return ps.stream().collect(Collectors.toList());
+		}
+		logger.info("Employees find");
+		List<Employee> e = repository.findByCompany(company);
+		logger.info("Employees size: " + e.size());
+		e.parallelStream().forEach(it -> {
+			map.putIfAbsent(it.getId(), it);
+		});
+		return e;
+	}
+
+	public Employee findById(Integer id) {
+		Employee e = map.get(id);
+		if (e != null)
+			return e;
+		e = repository.findOne(id);
+		map.put(id, e);
+		return e;
+	}
+	
+	public Employee add(Employee e) {
+		e = repository.save(e);
+		map.put(e.getId(), e);
+		return e;
+	}
+	
+}
