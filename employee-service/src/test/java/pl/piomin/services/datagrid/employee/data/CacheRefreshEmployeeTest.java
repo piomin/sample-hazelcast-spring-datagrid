9b330d05b675952b0036a92da2614f1549a8b157
Piotr Mińkowski <piotr_minkowski@wp.pl>
2017-08-06 11:40:55 +0200

    junit cache test

--- employee-service/src/test/java/pl/piomin/services/datagrid/employee/data/CacheRefreshEmployeeTest.java
+++ employee-service/src/test/java/pl/piomin/services/datagrid/employee/data/CacheRefreshEmployeeTest.java
@@ -6,8 +6,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.hazelcast.HazelcastAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -17,7 +15,6 @@ import pl.piomin.services.datagrid.employee.model.Employee;
 
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @RunWith(SpringRunner.class)
-@EnableAutoConfiguration(exclude = {HazelcastAutoConfiguration.class})
 public class CacheRefreshEmployeeTest {
 
 	protected Logger logger = Logger.getLogger(CacheRefreshEmployeeTest.class.getName());
@@ -29,17 +26,17 @@ public class CacheRefreshEmployeeTest {
 	
 	@Test
 	public void testModifyAndRefresh() {
-		Employee e = template.getForObject("http://localhost:3333/employees/{id}", Employee.class, 1);
-		Employee eMod = repository.findOne(1);
-		eMod.setPersonId(eMod.getPersonId()+1);
-		repository.save(eMod);
-		Employee e2 = template.getForObject("http://localhost:3333/employees/{id}", Employee.class, 1);
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		Employee[] e = template.getForObject("http://localhost:3333/employees/company/{company}", Employee[].class, "Test001");
+		for (int i = 0; i < e.length; i++) {
+			Employee eMod = repository.findOne(e[i].getId());
+			eMod.setPersonId(eMod.getPersonId()+1);
+			repository.save(eMod);
 		}
-		Assert.assertNotEquals(e.getPersonId(), e2.getPersonId());
+
+		Employee[] e2 = template.getForObject("http://localhost:3333/employees/company/{company}", Employee[].class, "Test001");
+		for (int i = 0; i < e2.length; i++) {
+			Assert.assertNotEquals(e[i].getPersonId(), e2[i].getPersonId());
+		}
+		
 	}
 }
