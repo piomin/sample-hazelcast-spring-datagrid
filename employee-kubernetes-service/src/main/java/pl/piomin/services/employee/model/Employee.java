041e8b5be017529f5650df3c09a721b523ceefb2
piomin <piotr.minkowski@gmail.com>
2023-02-08 13:19:29 +0100

    remove lombok

--- employee-kubernetes-service/src/main/java/pl/piomin/services/employee/model/Employee.java
+++ employee-kubernetes-service/src/main/java/pl/piomin/services/employee/model/Employee.java
@@ -1,29 +1,82 @@
 package pl.piomin.services.employee.model;
 
-import java.io.Serializable;
-
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import org.springframework.data.annotation.Id;
 
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
+import java.io.Serializable;
+import java.util.Objects;
+
 public class Employee implements Serializable {
 
 	@Id
 	private Long id;
-	@EqualsAndHashCode.Exclude
 	private Integer personId;
-	@EqualsAndHashCode.Exclude
 	private String company;
-	@EqualsAndHashCode.Exclude
 	private String position;
-	@EqualsAndHashCode.Exclude
 	private int salary;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getPersonId() {
+		return personId;
+	}
+
+	public void setPersonId(Integer personId) {
+		this.personId = personId;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" +
+				"id=" + id +
+				", personId=" + personId +
+				", company='" + company + '\'' +
+				", position='" + position + '\'' +
+				", salary=" + salary +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Employee employee = (Employee) o;
+
+		return Objects.equals(id, employee.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
 }
