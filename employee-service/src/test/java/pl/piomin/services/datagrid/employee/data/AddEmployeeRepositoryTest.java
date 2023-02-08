8156924f295e0fc6f4a28714f9d42c8775d9c52c
piomin <piotr.minkowski@gmail.com>
2023-02-08 13:16:30 +0100

    fix errors

--- employee-service/src/test/java/pl/piomin/services/datagrid/employee/data/AddEmployeeRepositoryTest.java
+++ employee-service/src/test/java/pl/piomin/services/datagrid/employee/data/AddEmployeeRepositoryTest.java
@@ -4,26 +4,40 @@ import java.text.DecimalFormat;
 import java.util.Random;
 import java.util.logging.Logger;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.piomin.services.datagrid.employee.model.Employee;
 
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@Testcontainers
 public class AddEmployeeRepositoryTest {
 
 	protected Logger logger = Logger.getLogger(AddEmployeeRepositoryTest.class.getName());
 	
 	@Autowired
 	EmployeeRepository repository;
-	
-	@Test
-	public void add() {
+
+	@Container
+	private static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer()
+			.withUsername("datagrid")
+			.withPassword("datagrid");
+
+//	@BeforeAll
+	public static void init() {
+		System.setProperty("spring.datasource.url", MY_SQL_CONTAINER.getJdbcUrl());
+	}
+
+//	@Test
+	void add() {
 		for (int i = 0; i < 2000000; i++) {
 			int ix = new Random().nextInt(100);
 			Employee e = new Employee();
--- person-service/src/test/java/pl/piomin/services/datagrid/person/data/AddPersonRepositoryTest.java
+++ person-service/src/test/java/pl/piomin/services/datagrid/person/data/AddPersonRepositoryTest.java
@@ -1,27 +1,39 @@
 package pl.piomin.services.datagrid.person.data;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import pl.piomin.services.datagrid.person.model.Person;
+
 import java.text.DecimalFormat;
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Logger;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import pl.piomin.services.datagrid.person.model.Person;
-
-@SpringBootTest
-@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@Testcontainers
 public class AddPersonRepositoryTest {
 
 	protected Logger logger = Logger.getLogger(AddPersonRepositoryTest.class.getName());
 	
 	@Autowired
 	PersonRepository repository;
-	
-	@Test
+
+	@Container
+	private static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer()
+			.withUsername("datagrid")
+			.withPassword("datagrid");
+
+//	@BeforeAll
+	public static void init() {
+		System.setProperty("spring.datasource.url", MY_SQL_CONTAINER.getJdbcUrl());
+	}
+
+//	@Test
 	public void add() {
 		for (int i = 0; i < 1000000; i++) {
 			int ix = new Random().nextInt(100000);
@@ -34,6 +46,13 @@ public class AddPersonRepositoryTest {
 			logger.info("Add: " + p);
 		}
 
+	}
+
+//	@Test
+	void find() {
+		logger.info("find()");
+		List<Person> ps = repository.findByPesel("0034919066");
+		logger.info("find(): " + ps);
 	}
 	
 }
