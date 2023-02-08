ff884535916bd079fe897c8d8e2609e87a005de2
piomin <piotr.minkowski@gmail.com>
2023-02-08 15:49:51 +0100

    adding tests

--- employee-service/src/test/java/pl/piomin/services/datagrid/employee/data/AddEmployeeRepositoryTest.java
+++ employee-service/src/test/java/pl/piomin/services/datagrid/employee/data/AddEmployeeRepositoryTest.java
@@ -1,24 +1,22 @@
 package pl.piomin.services.datagrid.employee.data;
 
-import java.text.DecimalFormat;
-import java.util.Random;
-import java.util.logging.Logger;
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.piomin.services.datagrid.employee.model.Employee;
 
+import java.text.DecimalFormat;
+import java.util.Random;
+import java.util.logging.Logger;
 
-//@SpringBootTest
-//@Testcontainers
+
+@SpringBootTest
+@Testcontainers
 public class AddEmployeeRepositoryTest {
 
 	protected Logger logger = Logger.getLogger(AddEmployeeRepositoryTest.class.getName());
@@ -27,18 +25,18 @@ public class AddEmployeeRepositoryTest {
 	EmployeeRepository repository;
 
 	@Container
-	private static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer()
+	private static final MySQLContainer MYSQL = new MySQLContainer()
 			.withUsername("datagrid")
 			.withPassword("datagrid");
 
-//	@BeforeAll
-	public static void init() {
-		System.setProperty("spring.datasource.url", MY_SQL_CONTAINER.getJdbcUrl());
+	@DynamicPropertySource
+	static void mysqlProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", MYSQL::getJdbcUrl);
 	}
 
-//	@Test
+	@Test
 	void add() {
-		for (int i = 0; i < 2000000; i++) {
+		for (int i = 0; i < 1000; i++) {
 			int ix = new Random().nextInt(100);
 			Employee e = new Employee();
 			e.setPersonId(i);
